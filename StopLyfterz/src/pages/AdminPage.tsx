30bd1f93c6b2427dee14ff4f8e851e6d8d31d7f1
Noah L <lauresno@uwplatt.edu>
2025-04-13 00:50:30 -0500

    Tidied up the Dashboard and Lyfter Card designs. Also added new CSS file for Button designs, so the code isn't repeated on every page that has a button.

--- StopLyfterz/src/pages/AdminPage.tsx
+++ StopLyfterz/src/pages/AdminPage.tsx
@@ -1,100 +1,100 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 //import '../assets/styles/BusinessPage.css';
-import '../assets/styles/Header.css';
+import "../assets/styles/Header.css";
 
 interface Post {
-    id: number;
-    name: string;
-    image: string;
-    description: string;
+  id: number;
+  name: string;
+  image: string;
+  description: string;
 }
 
 interface BusinessAccount {
-    businessName: string;
-    email: string;
-    id: number;
+  businessName: string;
+  email: string;
+  id: number;
 }
 
 const Admin: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [pendingAccounts, setPendingAccounts] = useState<BusinessAccount[]>([]);
+  const navigate = useNavigate();
 
-    const [posts, setPosts] = useState<Post[]>([]);
-    const [pendingAccounts, setPendingAccounts] = useState<BusinessAccount[]>([]);
-    const navigate = useNavigate();
+  const handleAccept = (id: number) => {
+    "something";
+  };
 
-    const handleAccept = (id: number) => {
-        'something'
-    };
+  const handleDeny = (id: number) => {
+    "something aswell";
+  };
 
-    const handleDeny = (id: number) => {
-        'something aswell'
-    };
-
-
-    return (
-        <div>
-            <header className="header-outer">
-	            <div className="header-inner responsive-wrapper">
-		            <h1>STOPLYFTERZ</h1>
-                    <div className='header-inner'>
-                        <h2>Admin Dashboard</h2>
-                    </div>
-		            <nav className="header-navigation">
-			            <a href="/">Home</a>
-			            <a href="#">About</a>
-			            <a href="#">Blog</a>
-			            <a href="#">Logout</a>
-		            </nav>
-	            </div>
-            </header>
-            {/* <header>
-                <h1>Admin Dashboard</h1>
-                <button onClick={() => navigate('/')} className='custom-button'>Home Page</button>
-                <button className='logout-button'>Logout</button>
-            </header> */}
-
-            <main className="main">
-            <div className="main-content responsive-wrapper">
-                <h2>Posted People</h2>
-                <hr></hr>
-                {posts.length > 0 ? (
-                    <ul>
-                        {posts.map(post => (
-                            <li key={post.id} style={{ marginBottom: '10px' }}>
-                                <img src={post.image} alt={post.name} style={{ width: '100px', marginRight: '10px' }} />
-                                <strong>{post.name}</strong>
-                                <p>{post.description}</p>
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>No posts available.</p>
-                )}
-
-        
-                <h2>Pending Business Accounts</h2>
-                <hr></hr>
-                {pendingAccounts.length > 0 ? (
-                    <ul>
-                        {pendingAccounts.map(account => (
-                            <li key={account.id} style={{ marginBottom: '10px' }}>
-                                <p>Email: {account.email}</p>
-                                <p>Business Name: {account.businessName}</p>
-                                <button onClick={() => handleAccept(account.id)} style={{ marginRight: '10px' }}>
-                                    Accept
-                                </button>
-                                <button onClick={() => handleDeny(account.id)}>Deny</button>
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>No pending business accounts.</p>
-                )}
-            </div>
-            </main>
+  return (
+    <div>
+      <header className="header-outer">
+        <div className="header-inner responsive-wrapper">
+          <nav className="header-navigation-logo">
+            <a href="/">STOPLYFTERZ</a>
+          </nav>
+          <div className="header-inner">
+            <h1>Dashboard</h1>
+          </div>
+          <nav className="header-navigation">
+            <a href="/">Home</a>
+            <a href="#">About</a>
+            <a href="#">Blog</a>
+            <a href="#">Logout</a>
+          </nav>
         </div>
-    
-    );
+      </header>
+
+      <main className="main">
+        <div className="main-content responsive-wrapper">
+          <h2>Posted People</h2>
+          <hr></hr>
+          {posts.length > 0 ? (
+            <ul>
+              {posts.map((post) => (
+                <li key={post.id} style={{ marginBottom: "10px" }}>
+                  <img
+                    src={post.image}
+                    alt={post.name}
+                    style={{ width: "100px", marginRight: "10px" }}
+                  />
+                  <strong>{post.name}</strong>
+                  <p>{post.description}</p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No posts available.</p>
+          )}
+
+          <h2>Pending Business Accounts</h2>
+          <hr></hr>
+          {pendingAccounts.length > 0 ? (
+            <ul>
+              {pendingAccounts.map((account) => (
+                <li key={account.id} style={{ marginBottom: "10px" }}>
+                  <p>Email: {account.email}</p>
+                  <p>Business Name: {account.businessName}</p>
+                  <button
+                    onClick={() => handleAccept(account.id)}
+                    style={{ marginRight: "10px" }}
+                  >
+                    Accept
+                  </button>
+                  <button onClick={() => handleDeny(account.id)}>Deny</button>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No pending business accounts.</p>
+          )}
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Admin;
--- StopLyfterz/src/pages/BusinessPage.tsx
+++ StopLyfterz/src/pages/BusinessPage.tsx
@@ -1,70 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom'
-import '../assets/styles/BusinessPage.css';
-
-
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../assets/styles/BusinessPage.css";
+import "../assets/styles/CustomButton.css";
 
 interface Post {
-    id: number;
-    name: string;
-    image: string;
-    description: string;
+  id: number;
+  name: string;
+  image: string;
+  description: string;
 }
 
-
 const Business: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const navigate = useNavigate();
+  interface BoxData {
+    src: string;
+    alt: string;
+    description: string;
+  }
 
-    const [posts, setPosts] = useState<Post[]>([]);
-    const navigate = useNavigate();
-        interface BoxData {
-            src: string;
-            alt: string;
-            description: string;
-        }    
+  return (
+    <div style={{ padding: "20px" }}>
+      <header>
+        <h1>Business Dashboard</h1>
+        <button onClick={() => navigate("/")} className="custom-button">
+          Home Page
+        </button>
+        <button className="logout-button">Logout</button>
+      </header>
 
+      <section>
+        <h2>Posted Lyfterz</h2>
+        <hr></hr>
+        {posts.length > 0 ? (
+          <ul>
+            {posts.map((post) => (
+              <li key={post.id} style={{ marginBottom: "10px" }}>
+                <img
+                  src={post.image}
+                  alt={post.name}
+                  style={{ width: "100px", marginRight: "10px" }}
+                />
+                <strong>{post.name}</strong>
+                <p>{post.description}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No posts available.</p>
+        )}
+      </section>
 
-    return (
-        <div style={{ padding: '20px' }}>
-            <header>
-                <h1>Business Dashboard</h1>
-                <button onClick={() => navigate('/')} className='custom-button'>Home Page</button>
-                <button className='logout-button'>Logout</button>
-            </header>
-            
-            <section>
-                <h2>Posted Lyfterz</h2>
-                <hr></hr>
-                {posts.length > 0 ? (
-                    <ul>
-                        {posts.map(post => (
-                            <li key={post.id} style={{ marginBottom: '10px' }}>
-                                <img src={post.image} alt={post.name} style={{ width: '100px', marginRight: '10px' }} />
-                                <strong>{post.name}</strong>
-                                <p>{post.description}</p>
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>No posts available.</p>
-                )}
-            </section>
+      <section>
+        <h2>Pending Lyfterz</h2>
+        <hr></hr>
+        <p>No Lyfterz pending.</p>
+      </section>
 
-            <section>
-                <h2>Pending Lyfterz</h2>
-                <hr></hr>
-                <p>No Lyfterz pending.</p>
-            </section>
-
-            <section>
-                <h2>Submit New Lyfter</h2>
-                <hr></hr>
-                <p>
-                    <button onClick={() => navigate('/form')} className='custom-button'>Create new lyfter card</button>
-                </p>
-            </section>
-
-        </div>    
-    );
+      <section>
+        <h2>Submit New Lyfter</h2>
+        <hr></hr>
+        <p>
+          <button onClick={() => navigate("/form")} className="custom-button">
+            Create new lyfter card
+          </button>
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default Business;
